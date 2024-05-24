3605f613fda8b5698ae90af38631af7aaf9d28c7
Kiyohara.K <hummingbird.kazu@gmail.com>
2024-05-24 19:08:56 +0900

    astro.config.mjsの内容を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://astrotestwabpage.netlify.app"
+    // Other configurations...
+    ,build: {
+    rollupOptions: {
+      external: ['@astrojs/rss']
+    }
+  }
 });
