8a4f3d7a3da4488efa22c3d17c7cf3244b0c3846
Kiyohara.K <hummingbird.kazu@gmail.com>
2024-05-28 16:08:02 +0900

    チュートリアル:コンテンツコレクションで拡張するが終了

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,17 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
   return rss({
     title: 'Astro学習者 | ブログ',
     description: 'Astroを学ぶ旅',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>ja-jp</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),    customData: `<language>ja-jp</language>`,
   });
 }
