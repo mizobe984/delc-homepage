efba4826004c3aaa0cf5e940eaa204e4d929d7cb
Kiyohara.K <hummingbird.kazu@gmail.com>
2024-07-29 19:38:50 +0900

    ヘッダーにアコーディオンメニュー追加

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,3 +3,15 @@ document.addEventListener('astro:page-load', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
   });
 });
+
+document.addEventListener('astro:page-load', () => {
+  document.querySelector('.company-hamburger').addEventListener('click', () => {
+    document.querySelector('.company-links-detail').classList.toggle('expanded-grid');
+  });
+});
+
+document.addEventListener('astro:page-load', () => {
+  document.querySelector('.services-hamburger').addEventListener('click', () => {
+    document.querySelector('.services-links-detail').classList.toggle('expanded-grid');
+  });
+});
