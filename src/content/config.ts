edcf7a5d864887b4459a8490f8cb9841a5b65efd
Kiyohara.K <hummingbird.kazu@gmail.com>
2024-09-05 18:40:57 +0900

    お知らせを一部修正

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,13 +6,10 @@ const postsCollection = defineCollection({
     schema: z.object({
       title: z.string(),
       pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
       image: z.object({
         url: z.string(),
         alt: z.string()
-      }),
-      tags: z.array(z.string())
+      }).optional(),
     })
 });
 // Export a single `collections` object to register your collection(s)
