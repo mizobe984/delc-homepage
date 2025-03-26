c4c16e185e0ce3b163415c2e515d843bca93452c
mizobe984 <am93090@gmail.com>
2025-03-26 15:45:38 +0900

    Modified js (Hamburger Icons & Mobile Menu)

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,11 @@
 document.addEventListener('astro:page-load', () => {
-  document.querySelector('.hamburger').addEventListener('click', () => {
+  const element = document.querySelector('.hamburger')
+  const alignJustify_icon = element.querySelector(".alignJustify-icon");
+  const x_icon = element.querySelector(".x-icon");
+  element.addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
+    alignJustify_icon.classList.toggle("open-icon");
+    x_icon.classList.toggle("close-icon");
   });
 });
 
@@ -10,19 +15,6 @@ document.addEventListener('astro:page-load', () => {
   const item = Array.from(main);
 
   item.forEach(function (element) {
-
-    element.addEventListener("click", function() {
-      const detail = element.querySelector(".links-detail");
-      const alignJustify_icon = element.querySelector(".alignJustify-icon");
-      const x_icon = element.querySelector(".x-icon");
-      if (detail) {
-        if (window.innerWidth <= 919) {
-          detail.classList.toggle("open-links-detail");
-          alignJustify_icon.classList.toggle("open-icon");
-          x_icon.classList.toggle("close-icon");
-        }
-      }
-    }, false);
     
     /*PC処理*/
     element.addEventListener("mouseover", function() {
